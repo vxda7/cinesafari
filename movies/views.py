346d7a2f0ee3621b2d2948f4ac92b5d5f1f17bef
vxda7 <vxda77@gmail.com>
2019-11-21 18:01:23 +0900

    데이터 뽑아내기완료

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import json
-from decouple import config
-import requests
-from datetime import datetime
 import sys
+import requests
+from decouple import config
+from datetime import datetime
+from .serializers import GenreSerializer, DirectorSerializer, MovieSerializer, ActorSerializer, UserSerializer
+
+
 
 # Create your views here.
 def datasave(request):
@@ -11,6 +14,7 @@ def datasave(request):
     thisyear = datetime.now().year
     MOVIE_KEY = config('MOVIE_KEY')
     BASIC_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json'
+    DETAIL_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json'
     MOVIE_URL = f'{BASIC_URL}?key={MOVIE_KEY}&openStartDt=2019&openEndDt={thisyear}&itemPerPage=10'
     movie_datas = requests.get(MOVIE_URL).json()
 
@@ -25,11 +29,18 @@ def datasave(request):
     }
 
     # 데이터 통합
+    movie_detail_datas = []
+    directors = set()
+    actors = set()
     idx = 0
     for movie_data in movie_datas['movieListResult']['movieList']:
         movie_name = movie_data['movieNm']
         naver_data = requests.get(f'{NAVER_URL}?query={movie_name}', headers=headers).json()
-        if naver_data['itmes']:
+        movie_code = movie_data['movieCd']
+        movie_detail_data = requests.get(f'{DETAIL_URL}?key={MOVIE_KEY}&movieCd={movie_code}').json()
+        movie_detail_datas.append(movie_detail_data)
+
+        if naver_data['items']:
             naver_score = naver_data['items'][0]['userRating']
         else:
             naver_score = 0
@@ -37,11 +48,20 @@ def datasave(request):
         if len(naver_data['items']) > 0:
             image_data = naver_data['items'][0]['image']
             movie_datas['movieListResult']['movieList'][idx]['image'] = image_data
-        
+        # 영화감독, 배우 추가            
+        if movie_data['directors']:
+            for one in movie_data['directors']:
+                directors.add(one['peopleNm'])
+        if movie_detail_data['movieInfoResult']['movieInfo']['actors']:
+            for one in movie_detail_data['movieInfoResult']['movieInfo']['actors']:
+                actors.add(one['peopleNm'])
         idx += 1
 
     context = {
         'MOVIE_URL': MOVIE_URL,
-        'movie_datas' : movie_datas
+        'movie_datas' : movie_datas,
+        'directors': directors,
+        'actors': actors,
+        'movie_detail_datas': movie_detail_datas,
     }
     return render(request, 'index.html', context)
